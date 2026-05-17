519de8f1ac5b55bc5918d11782df7d301229466a
Dimsky <dimasrizkia477@gmail.com>
2026-05-17 23:40:14 +0700

    ubah connector

--- Cahier/src/Model/Connector.java
+++ Cahier/src/Model/Connector.java
@@ -11,26 +11,28 @@ import java.sql.*;
  * @author LENOVO
  */
 public class Connector {
-    /* 
-        Menyimpan informasi database ke dalam sebuah variabel.
-        Pada contoh ini kita menggunakan database bernama "upnvy".
-     */
-    private static String jdbc_driver = "com.mysql.cj.jdbc.Driver";
-    private static String nama_db = "-";
-    private static String url_db = "jdbc:mysql://localhost:3306/" + nama_db;
-    private static String username_db = "root";
-    private static String password_db = "";
+    // private static String jdbc_driver = "com.mysql.cj.jdbc.Driver";
+    // private static String nama_db = "-";
+    // private static String url_db = "jdbc:mysql://localhost:3306/" + nama_db;
+    // private static String username_db = "root";
+    // private static String password_db = "";
+
+    private static String jdbc_driver = "org.sqlite.JDBC";
+    private static String nama_db = "cashier_ppbo.sqlite";
+    private static String url_db = "jdbc:sqlite:" + nama_db;
 
     static Connection conn;
-    
-    // Mencoba menghubungkan program kita dengan ke database MySQL.
+
+    // Mencoba menghubungkan program kita dengan database SQLite.
     public static Connection connect() {
         try {
             Class.forName(jdbc_driver);
-            
-            conn = DriverManager.getConnection(url_db, username_db, password_db);
 
-            System.out.println("MySQL Connected");
+            // conn = DriverManager.getConnection(url_db, username_db, password_db);
+
+            conn = DriverManager.getConnection(url_db);
+
+            System.out.println("SQLite Connected");
         } catch (ClassNotFoundException | SQLException exception) {
             System.out.println("Connection Failed: " + exception.getLocalizedMessage());
         }
